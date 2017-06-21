788416a344075c9e8b3d86433b0c4abfa49351cb
Devi Acharya <dacharya64@gmail.com>
2017-06-21 10:24:41 -0500

    Adding marker that spawns raondomly on game start

---
+++ Assets/Scripts/MarkerSpawn.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarkerSpawn : MonoBehaviour {
+    public float distance1 = 1;
+    public float distance2 = 2;
+    public float distance3 = 3;
+
+	// Use this for initialization
+	void Start () {
+
+        //sets initial position of marker
+        Random rand = new Random();
+        int distance = Random.Range(0, 3);
+		this.transform.position = new Vector3(Random.Range(-3, 3), 0, Random.Range(-3, 3));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
