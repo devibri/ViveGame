b5fed76fb73748faac826d0c26e32da215f91db1
Devi Acharya <dacharya64@gmail.com>
2017-06-22 16:54:10 -0500

    Updating Gitignore and adding scripts

---
+++ Assets/Scripts/Collider.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collider : MonoBehaviour {
+    private GameObject rig;
+    private GameObject cam;
+	// Use this for initialization
+	void Start () {
+        rig = GetComponent<GameObject>();
+        cam = GameObject.Find("Camera (head)"); 
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //sets the center of the collider to the camera location
+        rig.GetComponent<BoxCollider>().center = cam.GetComponent<Transform>().position;
+	}
+}
