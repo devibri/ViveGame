dabf763075f0a8c2db08fff434e70bf08bae827d
Devi Acharya <dacharya64@gmail.com>
2017-06-23 16:44:39 -0500

    Adding script that rotates arrow on marker

---
+++ Assets/Scripts/ArrowRotate.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowRotate : MonoBehaviour {
+
+    Random rand = new Random();
+    //ArrayList angleArray = new ArrayList();
+
+    int[] angleArray = new int[] { 0, 45, 90, 135, 180, 225, 270, 315 };
+    int randNum;
+
+
+    // Use this for initialization
+    void Start () {
+        randNum = Random.Range(0, 9);
+        this.transform.Rotate(0, angleArray[randNum], 0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
