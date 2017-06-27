eb5d2015e9cb0f984e0205d3c9d227a4d2985dfc
Devi Acharya <dacharya64@gmail.com>
2017-06-27 16:58:50 -0500

    making markers transparent

--- Assets/Scripts/ArrowRotate.cs
+++ Assets/Scripts/ArrowRotate.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ArrowRotate : MonoBehaviour {
-    public static int randNum = 0;
-    public static int randDegree = 0; 
+    public static int randNum;
+    public static int randDegree;
+    GameObject marker;
+    float markerX;
+    float markerZ;
 
-    Random rand = new Random();
+    //Random rand = new Random();
     //ArrayList angleArray = new ArrayList();
 
     int[] angleArray = new int[] { 0, 45, 90, 135, 180, 225, 270, 315 };
@@ -15,8 +18,29 @@ public class ArrowRotate : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
-        randNum = Random.Range(0, 9);
-        randDegree = angleArray[randNum];
+        
+        //if marker is in a certain position, prevent degree from being such that it points back to the origin
+        marker = GameObject.Find("FirstMarker");
+        markerX = marker.transform.position.x;
+        markerZ = marker.transform.position.z;
+
+        if (((marker.transform.position.z < 0) && (marker.transform.position.x == 0)) || ((markerZ > 0) && (markerX == 0)))
+        {
+            GetRandDegree(180, 0);
+        }
+        else if (((markerZ < 0) && (markerX < 0)) || ((markerZ > 0) && (markerX > 0)))
+        {
+            GetRandDegree(225, 45);
+        }
+        else if (((markerZ == 0) && (markerX < 0)) || ((markerZ == 0) && (markerX > 0)))
+        {
+            GetRandDegree(270, 90);
+        }
+        else if (((markerZ > 0) && (markerX < 0)) || ((markerZ < 0) && (markerX > 0)))
+        {
+            GetRandDegree(315, 135);
+        }
+
         this.transform.Rotate(0, randDegree, 0);
     }
 	
@@ -24,4 +48,16 @@ public class ArrowRotate : MonoBehaviour {
 	void Update () {
 		
 	}
+
+    public void GetRandDegree(int wrongDegree, int wrongDegree2) {
+        do
+        {
+            randNum = Random.Range(0, 8);
+            randDegree = angleArray[randNum];
+        } while (randDegree == wrongDegree || randDegree == wrongDegree2);
+
+
+
+
+    }
 }
