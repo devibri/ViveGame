479c578ea51a9db283ae053d4e3be601687a2404
Bustamante <emanuelb@iastate.edu>
2017-06-27 10:37:44 -0500

    Figured out why the angle of rotation is wrong. Wrote code to fix it. Not tested yet.

--- Assets/Scripts/TrackpadRotation.cs
+++ Assets/Scripts/TrackpadRotation.cs
@@ -2,44 +2,51 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TrackpadRotation : MonoBehaviour {
+public class TrackpadRotation : MonoBehaviour
+{
     // Defining and instantiating a reference the controller 
     private SteamVR_TrackedObject trackedObj;
-    private SteamVR_Controller.Device Controller {
+    private SteamVR_Controller.Device Controller
+    {
         get { return SteamVR_Controller.Input((int)trackedObj.index); }
     }
-
-    // private GameObeject controller; 
-
 
     // Keep track of the location of the thumb on the trackpad and the
     // direction of the player
     Vector2 touchPosition, playerDirection;
     private GameObject camera;
+    bool write;
 
 
-    void Awake(){
+    void Awake()
+    {
         trackedObj = GetComponent<SteamVR_TrackedObject>();
     }
 
     // Use this for initialization
-    void Start () {
-        camera = GameObject.Find("Camera (head)");
-        // controller = GameObject.Find("Controller (right)");
+    void Start()
+    {
+        camera = GameObject.Find("Camera (eye)");
+        write = false;
     }
 
     // Update is called once per frame
-    void Update () {
+    void Update()
+    {
+        //Debug.Log("You're facing: " + camera.transform.forward.ToString());
+        //Debug.Log("You're pointing: " + Controller.GetAxis().ToString()); // Working as expected
         Debug.Log(getAngle());
     }
 
     // Get the angle the play would rotate
-    float getAngle(){
-        // Update the two vectors
-        touchPosition = Controller.GetAxis();
+    float getAngle()
+    {
         playerDirection = camera.transform.forward;
+        Vector2 trackpadDirection = Controller.GetAxis();
+        float offsetAngle = Vector.Angle(playerDirection, Vector2.up);
+        Vector2 turnVector = Quaternion.AngleAxis(offsetAngle, Vector3.forwward) * trackpadDirection;
 
         // Find the angle between them
-        return Vector2.Angle(playerDirection, touchPosition);
+        return Vector2.Angle(playerDirection, turnVector);
     }
 }
