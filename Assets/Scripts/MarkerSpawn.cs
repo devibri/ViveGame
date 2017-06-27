7c9c9ee61705a9428954e1ad4692143792c64db9
Devi Acharya <dacharya64@gmail.com>
2017-06-27 10:59:22 -0500

    marker1 no longer spawns at origin

--- Assets/Scripts/MarkerSpawn.cs
+++ Assets/Scripts/MarkerSpawn.cs
@@ -3,11 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MarkerSpawn : MonoBehaviour {
+    int xRange;
+    int zRange; 
     // Use this for initialization
 
     void Start () {
         //sets initial position of first marker
-		this.transform.position = new Vector3(Random.Range(-3, 4), 0, Random.Range(-3, 4));
+        xRange = 0;
+        zRange = 0;
+
+        while (xRange == 0 && zRange == 0) {
+            xRange = Random.Range(-3, 4);
+            zRange = Random.Range(-3, 4);
+        } 
+
+
+		this.transform.position = new Vector3(xRange, 0, zRange);
 	}
 	
 	// Update is called once per frame
